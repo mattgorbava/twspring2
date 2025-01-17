688cc27cf68e40614394019611177212ecd2ab0a
Matei Ionescu <imatei2003@gmail.com>
2025-01-17 13:01:49 +0200

    Implemented admin screen, oauth2 using google and started implementing user screen and album database

--- src/main/java/com/example/twspring2/Twspring2Application.java
+++ src/main/java/com/example/twspring2/Twspring2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.example.twspring2.database.repository")
+@EnableJpaRepositories("com.example.twspring2.database.users.repository")
 public class Twspring2Application {
 
 	public static void main(String[] args) {
